dc10650b56829f8c2fa97e2e83d8d1d4d0e51739
wisdm-nikhil <nikhil.mhaske@gmail.com>
2025-11-23 00:36:13 +0530

    App Upload Done

---
+++ userapp/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let users = [];
+
+app.post("/users", (req, res) => {
+  const user = req.body;
+  users.push(user);
+
+  res.json({
+    status: "success",
+    message: "User added successfully",
+    data: user
+  });
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
+
+
+// HOW TO RUN THIS APPLICATION:
+// Step 1: Open terminal inside this folder
+// Step 2: Run → node app.js
+
+// TEST USING TERMINAL (curl):
+// curl -X POST -H "Content-Type: application/json" -d '{"name":"Vaibhav","email":"vaibhav@gmail.com"}' http://localhost:3000/users
+
+// VIEW ALL USERS:
+// http://localhost:3000/users
